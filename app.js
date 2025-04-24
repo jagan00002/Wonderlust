72b71a0f29b2244f6c95bd2306479b5539e06d68
jagannath G H <140091429+jagan00002@users.noreply.github.com>
2025-04-24 19:42:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ app.use("/",users)
 // if requiered route not entered correctly
 
 app.all("*",(req,res,next)=>{
-    next(new ExpressError(404,"page not found fuck youu!!"))
+    next(new ExpressError(404,"page not found!!"))
 })
 // middleware for custom handle error
 
